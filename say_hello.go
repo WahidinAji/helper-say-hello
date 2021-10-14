da6d9071551d3fda13f9092dbd5e6b0628a9556d
WahidinAji <a17wahidin@gmail.com>
2021-10-15 05:56:09 +0700

    update sampel package

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,26 @@
 package helper
 
+/*
+remember this one, if the package name (name of function or variable) using Capital in the first words, then it can access on other package.
+if the package name using lowercase, then it can't be accessed by other package
+*/
+
+var version = "1.0.0" //can't access by other package
+var Application = "golang" //it can
+
+/*
+function sayGoodBy can access on other package
+ */
+
+func sayGoodBy() string {
+	return "this is sample the function using lowercase can't be accessed by other package"
+}
+
+
+/*
+function SayHello can access on other package
+ */
+
 func SayHello() string {
 	return "hai string"
 }
